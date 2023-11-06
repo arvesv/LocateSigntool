2f1da92710d48fb324012cf2c3cf3ce02d3bb070
Arve Svendsen <657470+arvesv@users.noreply.github.com>
2023-11-06 11:20:54 +0100

    Update locatesigntool.ps1

--- locatesigntool.ps1
+++ locatesigntool.ps1
@@ -2,7 +2,7 @@
 $tools = (dir signtool.exe -path 'C:\Program Files (x86)\Windows Kits\10\bin\' -Recurse).FullName
 foreach ($path in $tools) {
   
-    if($path.Contains('x64')) {
+    if($path.Contains('x86')) {
         $path
         break
     }
